d0bcded2d29236f67faea04068d7b61cce21d90f
Keyvan <skhprogramming@gmail.com>
2025-07-30 12:19:31 +0330

    test: add tests for TagList

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import TagList from "../../src/components/TagList";
+
+describe("TagList", () => {
+	it("should render tags", async () => {
+		render(<TagList />);
+
+		// defaults time is 1s
+		await waitFor(() => {
+			const listItems = screen.getAllByRole("listitem");
+			expect(listItems.length).toBeGreaterThan(0);
+		});
+
+		const listItems = await screen.findAllByRole("listitem");
+		expect(listItems.length).toBeGreaterThan(0);
+	});
+});
