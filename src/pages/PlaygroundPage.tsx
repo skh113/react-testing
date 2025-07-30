0044729c0098c090157e4109e1ae898520b15848
Keyvan <skhprogramming@gmail.com>
2025-07-30 13:25:47 +0330

    chore: update package dependencies and enhance PlaygroundPage with new components

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,16 +1,22 @@
 import ExpandableText from "../components/ExpandableText";
 import Onboarding from "../components/Onboarding";
+import OrderStatusSelector from "../components/OrderStatusSelector";
 import SearchBox from "../components/SearchBox";
+import TagList from "../components/TagList";
 import TermsAndConditions from "../components/TermsAndConditions";
+import ToastDemo from "../components/ToastDemo";
 
 const PlaygroundPage = () => {
 	return (
 		<section className="gap-6 flex flex-col">
 			<Onboarding />
-			<SearchBox onChange={(text) => console.log(text)} />
+			<SearchBox onChange={console.log} />
 			<TermsAndConditions />
 			<ExpandableText text="Lorem!" />
 			<ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos et incidunt vero velit reiciendis cum ea cupiditate labore maxime tenetur reprehenderit, eligendi architecto consequatur est dolor aliquam recusandae, esse repudiandae optio. Hic et, ut nisi nihil sed ullam natus necessitatibus explicabo magnam! Delectus dolorum commodi suscipit aliquam omnis officia consequuntur quisquam. Necessitatibus nesciunt qui quam eveniet hic minima id illum corporis quis repudiandae repellat soluta ex eligendi tenetur, maiores ipsam aperiam aut vitae, ea autem. Ratione exercitationem sapiente distinctio nesciunt, obcaecati sint modi, similique labore ducimus, ipsa adipisci nobis unde. Quia soluta accusamus, dolores quis architecto voluptas vel modi harum!" />
+			<TagList />
+			<ToastDemo />
+			<OrderStatusSelector onChange={console.log} />
 		</section>
 	);
 };
